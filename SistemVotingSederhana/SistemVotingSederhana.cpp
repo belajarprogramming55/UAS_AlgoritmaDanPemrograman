067e49638148b8c9a7cbe2dff7978588533026e0
belajarprogramming55 <penyuimuts12@gmail.com>
2026-01-06 18:26:24 +0700

    brimbambum

--- SistemVotingSederhana/SistemVotingSederhana.cpp
+++ SistemVotingSederhana/SistemVotingSederhana.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
 int main() {
-    cout << "Fufifa";
+    map<string, int> pemilih;
+    int totalpemilih;
+
+    cout << "=== SISTEM VOTING MEREK MOBIL ===" << endl;
+    cout << "Masukkan Jumlah Pemilih: ";
+    cin >> totalpemilih;
+    cin.ignore();
+
+    for (int i = 1; i <= totalpemilih; i++) {
+        string merek;
+        cout << "Pemilih ke-" << i << ", Pilih Merek mobil: ";
+        getline(cin, merek);
+        pemilih[merek]++;
+    }
+    cout << "\n=== Hasil VOTING ===" << endl;
+    for (auto& pemilih : pemilih) {
+        cout << pemilih.first << ": " << pemilih.second << " Suara" << endl;
+    }
     return 0;
 }
